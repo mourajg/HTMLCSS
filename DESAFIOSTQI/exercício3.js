be82e3e1d915423b9d20bf161c1ee3571173b95e
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-05 07:37:50 -0300

    Create exercício3.js

---
+++ DESAFIOSTQI/exercício3.js
@@ -0,0 +1,11 @@
+>>>
+let input = gets ()
+let qtdEntradas = parseInt (input)
+let melhor = 999
+for (let i = 0; i < qtdEntradas; i ++) {
+	input = gets ()
+	let tempo = parseFloat (input)
+	if (tempo < melhor) melhor = tempo;    	
+}
+print (melhor)
+>>>
