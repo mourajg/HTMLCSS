ee08b0550ba157530227bbb1bb6a0beb6b73e73f
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-05 08:48:26 -0300

    Update script.js

--- GERADOR/script.js
+++ GERADOR/script.js
@@ -1,16 +1,16 @@
 >>>
-let sliderElement = document.querySelector ("#slider");
-let buttonElement = document.querySelector ("#button");
-let sizePassword = document.querySelector ("#valor");
-let password = document.querySelector ("#password");
-let containerPassword = document.querySelector ("#container-password");
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789";
 let novaSenha = "";
 sizePassword.innerHTML = sliderElement.value;
-slider.oninput = function () {
+slider.oninput = function() {
 	sizePassword.innerHTML = this.value;
 }
-function generatePassword () {
+function generatePassword() {
 	let pass = "";
 	for (let i = 0, n = charset.length; i < sliderElement.value; ++ i) {
 		pass += charset.charAt (Math.floor (Math.random () * n));
